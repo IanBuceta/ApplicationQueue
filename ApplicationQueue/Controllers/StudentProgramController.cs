0e16d36712871f42fd2372b66f18d33f64f5573d
IanManuel BucetaSbrizza <IanManuel.BucetaSbrizza@Alumnos.uai.edu.ar>
2020-01-16 21:31:16 -0300

    Add project files.

---
+++ ApplicationQueue/Controllers/StudentProgramController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationQueue.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationQueue.Controllers
+{
+    public class StudentProgramController : Controller
+    {
+        static Queue<StudentProgram> studentPrograms = new Queue<StudentProgram>();
+        // GET: StudentProgram
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: StudentProgram/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentProgram/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentProgram/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(int id, string teamName)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                studentPrograms.Enqueue(new StudentProgram(id, teamName));
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentProgram/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentProgram/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, string newteamName, string newSrc)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var student = studentPrograms.ToList().Find(s => s.Id == id);
+                student.TeamName = newteamName;
+                student.Src = newSrc;
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentProgram/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentProgram/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete()
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                studentPrograms.Dequeue();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ApplicationQueue/Models/StudentProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationQueue.Models
+{
+    public class StudentProgram
+    {
+        public StudentProgram(int id, string teamName)
+        {
+            this.Id = id;
+            this.TeamName = teamName;
+        }
+
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public string Src { get; set; }
+        public bool IsRunning {
+            get
+            {
+                return CheckIfRunning();
+            }
+        }
+
+        private bool CheckIfRunning()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
