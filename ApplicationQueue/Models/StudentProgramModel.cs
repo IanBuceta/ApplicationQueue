3c72880e54ead35fa07b880c0c3d3e7dc4b5a293
IanManuel BucetaSbrizza <IanManuel.BucetaSbrizza@Alumnos.uai.edu.ar>
2020-01-18 17:38:36 -0300

    Renamed from StudentProgram

---
+++ ApplicationQueue/Models/StudentProgramModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationQueue.Models
+{
+    public class StudentProgram
+    {
+        public StudentProgram(uint id, string teamName, string src)
+        {
+            this.Id = id;
+            this.TeamName = teamName;
+            this.Src = src;
+        }
+
+        public uint Id { get; }
+        public string TeamName { get; set; }
+        public string Src { get; set; }
+        public bool IsRunning {
+            get
+            {
+                return CheckIfRunning();
+            }
+        }
+
+        private bool CheckIfRunning()
+        {
+            return false;
+        }
+    }
+}
